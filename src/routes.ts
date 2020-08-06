b7cae89dbd22f471588b10dddb94578530d3faeb
Uéslei Suptitz <ueslei.suptitz@paipe.co>
2020-08-06 19:49:58 -0300

    trx

--- src/routes.ts
+++ src/routes.ts
@@ -11,36 +11,33 @@ interface scheduleItem {
 }
 
 routes.post("/classes", async (req, res) => {
+  const trx = await db.transaction();
   try {
     const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
-    const insertedUsersIds = await db("users").insert({
+    const insertedUsersIds = await trx("users").insert({
       name,
       avatar,
       whatsapp,
       bio,
     });
-
     const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await db("classes").insert({
+    const insertedClassesIds = await trx("classes").insert({
       subject,
       cost,
       user_id,
     });
-
     const class_id = insertedClassesIds[0];
-
     const classSchedule = schedule.map((s: scheduleItem) => ({
       class_id,
       week_day: s.week_day,
       from: convertHourToMinutes(s.from),
       to: convertHourToMinutes(s.to),
     }));
-
-    await db("class_schedule").insert(classSchedule);
-
+    await trx("class_schedule").insert(classSchedule);
+    await trx.commit();
     return res.send();
   } catch (error) {
+    await trx.rollback();
     res.status(500).send({
       error,
     });
